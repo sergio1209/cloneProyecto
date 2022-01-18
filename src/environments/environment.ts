ae711af0c9b4cd2815c552056d7f8b0208c9e7f0
Royber <roybermanjarrez@gmail.com>
2022-01-18 17:02:25 -0500

    http-generic and model and env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+require('dotenv').config();
 
 export const environment = {
-  production: false
+  production: false,
+  BACKEND: process.env.BACKEND
 };
 
 /*
